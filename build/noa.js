5ee5003fdb79fcae4929e9c1c79d74a983249920
Glitch (mineweb) <none>
2020-04-27 10:51:14 +0000

    🎻🛋 Checkpoint ./build/noa.js:15400078/1647

--- build/noa.js
+++ build/noa.js
@@ -19,7 +19,13 @@ export function startNoa(noaOpts) {
   noa.registry.registerMaterial('dirt', brownish, textureURL)
   noa.registry.registerMaterial('grass', greenish, textureURL)
   
+  // block types and their material names
+  var dirtID = noa.registry.registerBlock(1, { material: 'dirt' })
+  var grassID = noa.registry.registerBlock(2, { material: 'grass' })
+  
   noa.world.maxChunksPendingCreation = 9999;
+  
+  
   
   noa.world.on('worldDataNeeded', function (id, data, x, y, z) {
     window.setTimeout(function() { // Just testing
@@ -32,13 +38,14 @@ export function startNoa(noaOpts) {
       for (var i = 0; i < data.shape[0]; i++) {
         for (var j = 0; j < data.shape[1]; j++) {
           for (var k = 0; k < data.shape[2]; k++) {
-            var voxelID = chunksToLoad[`${x / 16}|${y / 16}|${z / 16}`].getBlock(new Vec3(i, y + j, k));
+            // var voxelID = chunksToLoad[`${x / 16}|${y / 16}|${z / 16}`].getBlock(new Vec3(k, y + j, i)); // x and z are reversed because otherwise it looks wrong
             // console.log("ID:", voxelID);
-            if (voxelID.type == 0) {
+            /* if (voxelID.type == 0) {
               voxelID = 0;
             } else {
               voxelID = 1;
-            }
+            } */
+            var voxelID = getVoxelID(x + i, y + j, z + k)
             data.set(i, j, k, voxelID);
           }
         }
