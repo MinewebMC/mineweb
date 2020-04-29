eb8e6c9d4f5e822e3e31eaf81a350a5eca57d5e7
Glitch (mineweb-dev) <none>
2020-04-29 10:34:53 +0000

    💠📅 Checkpoint ./server/server.js:15400078/518

--- server/server.js
+++ server/server.js
@@ -6,6 +6,19 @@
   // Create our app
   var app = express();
 
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.get('Origin') || '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Expose-Headers', 'Content-Length');
+  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+  if (req.method === 'OPTIONS') {
+    return res.send(200);
+  } else {
+    return next();
+  }
+});
+
   app.use(netApi());
   app.use(express.static('./'))
 
